bfdc435869bdadf304d2ea9371d136dc97869512
cjredmond <connor.redmond@gmail.com>
2017-01-23 15:10:08 -0500

    added team model and data migration

--- nfl/models.py
+++ nfl/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    city = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    wins = models.IntegerField()
+    losses = models.IntegerField()
+    ties = models.IntegerField()
+    div = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- nfl/views.py
+++ nfl/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from nfl.models import Team
 
-# Create your views here.
+class IndexView(ListView):
+    model = Team
