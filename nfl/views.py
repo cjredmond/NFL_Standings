78a16e60dbb2a12dfe8d722ae9eeac0edfe4afbe
cjredmond <connor.redmond@gmail.com>
2017-01-23 16:20:03 -0500

    added some bootstrap styles

--- nfl/views.py
+++ nfl/views.py
@@ -4,3 +4,7 @@ from nfl.models import Team
 
 class IndexView(ListView):
     model = Team
+
+    # def get_context_data(self):
+    #     context = super().get_context_data()
+    #     return context
